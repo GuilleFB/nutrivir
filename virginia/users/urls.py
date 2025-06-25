1db75c17b3226f47f873c89df0ce07f0d6c67ec6
Guillermo Follana Berná <gfollana@apsl.net>
2025-06-25 22:06:35 +0200

    refactor: update URL patterns and remove unused templates

--- virginia/users/urls.py
+++ virginia/users/urls.py
@@ -5,13 +5,9 @@ from .views import user_detail_view
 from .views import user_redirect_view
 from .views import user_update_view
 
-def index(request):
-    return render(request, "index.html")
-
 app_name = "users"
 urlpatterns = [
     path("~redirect/", view=user_redirect_view, name="redirect"),
     path("~update/", view=user_update_view, name="update"),
     path("<int:pk>/", view=user_detail_view, name="detail"),
-    path("index/", view=index, name="index"),
 ]
